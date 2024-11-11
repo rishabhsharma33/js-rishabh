0bb5efc9f2fe6ed41a3580ada24ac495185e1d69
Rishabh Sharma <137682358+rishabhsharma33@users.noreply.github.com>
2024-11-11 05:31:47 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,30 @@
+// const name = "Rishabh"
+// const goal = "Software Engineer"
+
+// console.log("Hello my name is " + name + " and my goal is to become a " + goal + ".");
+// This is the old way to concatenate strings.
+
+// console.log(`Hello my name is ${name} and my goal is to become a ${goal}.`);
+//It will also output the same thing but this is more readable and preferred way. This will be used while development.
+
+// const gameName = new String('Sekiro')
+// console.log(gameName.__proto__);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.length);
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('r'));
+
+// const stringOne = "             Rishabh    "
+// console.log(stringOne);
+// console.log(stringOne.trim());
+
+
+// const url = "https://rishabh.com/rishabh%20sharma"
+// console.log(url.replace("%20","-"));
+// console.log(url.includes("rishabh"));
+
+
+// const sentence = "My name is rishabh sharma and I am going to split this string with the help of split() method on the bases of spaces."
+
+// console.log(sentence.split(" "));
+//This string will be converted to an array
